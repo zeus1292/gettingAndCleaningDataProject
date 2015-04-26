1b29b21f706542d40aa689fc1f6c792f5fc87d30
Akshay Kumar <akshay92k@gmail.com>
2015-04-27 04:18:23 +0530

    R Script for the project

--- run_analysis.R
+++ run_analysis.R
@@ -2,30 +2,24 @@ library(plyr)
 
 # Merge the training and test sets to create one data set
 
+#Training data
 x_train <- read.table("train/X_train.txt")
 y_train <- read.table("train/y_train.txt")
 subject_train <- read.table("train/subject_train.txt")
 
+#Test data
 x_test <- read.table("test/X_test.txt")
 y_test <- read.table("test/y_test.txt")
 subject_test <- read.table("test/subject_test.txt")
 
-# create 'x' data set
 x_data <- rbind(x_train, x_test)
-
-# create 'y' data set
 y_data <- rbind(y_train, y_test)
-
-# create 'subject' data set
 subject_data <- rbind(subject_train, subject_test)
 
 # Extract only the measurements on the mean and standard deviation for each measurement
 
 features <- read.table("features.txt")
-
-# get only columns with mean() or std() in their names
 meanStdFeatures <- grep("-(mean|std)\\(\\)", features[, 2])
-
 x_data <- x_data[, mean_and_std_features]
 
 # correct the column names
